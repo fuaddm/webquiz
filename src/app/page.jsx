16b5dc177781b8fa0486c6ac48cc15aeca3a40e3
Fuad Damirchiev <fuad.damirchiyev@azerimed.com>
2024-07-18 15:55:36 +0400

    homepage design 90%

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,31 @@
+import CategoryCard from '@/components/CategoryCard';
+
 export default async function Home() {
-  const resp = await fetch('http://127.0.0.1:3000/api/category');
-  const data = await resp.json();
-  console.log(data);
-  return <div className="min-h-screen bg-white"></div>;
+  // const resp = await fetch('http://127.0.0.1:3000/api/category');
+  // let data = null;
+  // if (resp.ok) {
+  //   data = await resp.json();
+  // }
+  const data = {
+    categories: [
+      { _id: '66979a6022116d3fc7f83676', name: 'HTML', questionCount: 3 },
+      { _id: '66979a8122116d3fc7f83678', name: 'CSS', questionCount: 0 },
+      {
+        _id: '66979a8822116d3fc7f8367a',
+        name: 'JAVASCRIPT',
+        questionCount: 0,
+      },
+    ],
+  };
+  return (
+    <div className="min-h-screen bg-[#1a1d24]">
+      <div className="container mx-auto flex h-screen flex-col items-center py-20">
+        <div className="mb-20">
+          <div className="mb-6 text-center text-6xl font-bold text-white">Web Quiz: Dive into HTML, CSS & JS</div>
+          <div className="text-center text-xl font-normal text-slate-400">Test your knowledge and skills in web development with our interactive quiz! Dive into three essential categories:</div>
+        </div>
+        <div className="grid w-full place-items-center">{data && <CategoryCard categories={data.categories} />}</div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/CategoryCard.jsx
@@ -0,0 +1,79 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import styles from './category.module.css';
+
+const CategoryCard = ({ categories }) => {
+  const [selected, setSelected] = useState(0);
+
+  useEffect(() => {
+    function arrowEvents(e) {
+      if (e.code == 'ArrowRight') {
+        setSelected((prev) => {
+          if (prev < 2) {
+            return ++prev;
+          }
+          return prev;
+        });
+      } else if (e.code == 'ArrowLeft') {
+        setSelected((prev) => {
+          if (prev > 0) {
+            return --prev;
+          }
+          return prev;
+        });
+      }
+    }
+    window.addEventListener('keydown', arrowEvents);
+
+    return () => window.removeEventListener('keydown', arrowEvents);
+  }, []);
+
+  return (
+    <div className={`${styles.card} w-[400px] overflow-hidden rounded-3xl bg-[#24272e] p-8`}>
+      <div className="relative mb-6 flex h-[400px] items-center justify-center">
+        <div
+          style={{ transform: `translateX(-${selected * 120}px)` }}
+          className="absolute left-[120px] top-0 z-50 flex h-full w-full items-center justify-center gap-8 transition-all">
+          {categories.map((item, index) => {
+            return (
+              <div
+                key={item._id}
+                onClick={() => setSelected(index)}
+                className={`${categories[selected].name == item.name ? styles.logoCircle : styles.logoCircle2} grid h-[88px] min-h-[88px] w-[88px] min-w-[88px] cursor-pointer place-items-center rounded-full bg-[#24272e] transition-all delay-150 duration-0`}>
+                <img
+                  src={`assets/${item.name}.svg`}
+                  width={80}
+                  height={80}
+                  alt={item.name}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className={`${styles.circleInCenter} relative z-40 h-28 w-28 rounded-full border-2 border-white/60 p-2.5`}>
+          <div className={`${styles.circle} absolute left-1/2 top-0 grid h-4 w-4 -translate-x-1/2 -translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
+            <div className="h-2 w-2 rounded-full bg-white"></div>
+          </div>
+          <div className={`${styles.circle} absolute bottom-0 left-1/2 grid h-4 w-4 -translate-x-1/2 translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
+            <div className="h-2 w-2 rounded-full bg-white"></div>
+          </div>
+          <div className="absolute left-1/2 top-0 h-[150px] w-[1px] -translate-x-1/2 -translate-y-full bg-gradient-to-t from-white/40"></div>
+        </div>
+        <div className="absolute left-0 top-0 z-30 h-full w-full">
+          <div className="absolute left-1/2 top-1/2 aspect-square w-[320px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/10"></div>
+          <div className="absolute left-1/2 top-1/2 aspect-square w-[240px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/30 bg-white/5"></div>
+          <div className={`${styles.squareShadow} absolute left-0 top-1/2 h-[130px] w-full -translate-y-[30px] rounded-full bg-[#24272e]`}></div>
+        </div>
+      </div>
+      <div className="relative z-50">
+        <div className="mb-1 text-2xl font-semibold text-white/90">{categories[selected].name}</div>
+        <div className="mb-5 text-lg font-normal text-white/70">Test your {categories[selected].name} skills from basics to advanced!</div>
+        <button className={`${styles.button} w-fit rounded-full px-8 py-3`}>
+          <span className="text-lg font-medium text-white/80">Start quiz</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryCard;
