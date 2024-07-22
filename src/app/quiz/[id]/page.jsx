e7c8c32f30dad5e54cf9a19a9e4ada4da52b89b7
Fuad Damirchiev <fuad.damirchiyev@azerimed.com>
2024-07-22 08:34:24 +0400

    dev

--- src/app/quiz/[id]/page.jsx
+++ src/app/quiz/[id]/page.jsx
@@ -1,23 +1,11 @@
 import QuizCard from '@/components/QuizCard';
-import ArrowV2 from '@/svg/ArrowV2';
-import SvgTimer from '@/svg/Timer';
 
-export default function Home() {
+export default async function Home({ params }) {
+  const resp = await fetch(`http://127.0.0.1:3000/api/question?categoryId=${params.id}`);
+  const data = await resp.json();
   return (
-    <div className="container mx-auto grid justify-center px-6 py-10">
-      <div className="max-w-[600px]">
-        <div className="mb-16 grid grid-cols-3 items-center gap-4">
-          <div className="w-fit cursor-pointer rounded-xl p-2 transition duration-300 hover:bg-white/10">
-            <ArrowV2 className="h-6 w-6 fill-white" />
-          </div>
-          <div className="text-center text-xl font-normal text-white">3/20</div>
-          <div className="flex items-center justify-end gap-1">
-            <SvgTimer className="h-5 w-5 stroke-green-600" />
-            <span className="text-xl font-normal text-green-600">14:57</span>
-          </div>
-        </div>
-        <QuizCard />
-      </div>
+    <div className="container mx-auto grid h-screen justify-center px-6 py-10">
+      <QuizCard data={data} />
     </div>
   );
 }
--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,49 +1,36 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import styles from './category.module.css';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const CategoryCard = ({ categories }) => {
+  const router = useRouter();
   const [selected, setSelected] = useState(0);
-  const [touchCords, setTouchCords] = useState({
-    touchStartX: null,
-    touchEndX: null,
-  });
 
-  function touchStart(e) {
-    setTouchCords((prevCords) => {
-      return {
-        ...prevCords,
-        touchStartX: e.changedTouches[0].clientX,
-      };
+  function goRight() {
+    setSelected((prev) => {
+      if (prev < categories.length - 1) {
+        return ++prev;
+      }
+      return prev;
     });
   }
-  function touchEnd(e) {
-    setTouchCords((prevCords) => {
-      if (prevCords.touchStartX - e.changedTouches[0].clientX > 0) {
+  function goLeft() {
+    setSelected((prev) => {
+      if (prev > 0) {
+        return --prev;
       }
-      return {
-        ...prevCords,
-        touchEndX: e.changedTouches[0].clientX,
-      };
+      return prev;
     });
   }
 
   useEffect(() => {
     function arrowEvents(e) {
       if (e.code == 'ArrowRight') {
-        setSelected((prev) => {
-          if (prev < 2) {
-            return ++prev;
-          }
-          return prev;
-        });
+        goRight();
       } else if (e.code == 'ArrowLeft') {
-        setSelected((prev) => {
-          if (prev > 0) {
-            return --prev;
-          }
-          return prev;
-        });
+        goLeft();
       }
     }
     window.addEventListener('keydown', arrowEvents);
@@ -51,54 +38,75 @@ const CategoryCard = ({ categories }) => {
     return () => window.removeEventListener('keydown', arrowEvents);
   }, []);
 
+  function submit() {
+    router.push(`/quiz/${categories[selected]._id}`);
+  }
+
   return (
-    <div className={`${styles.card} w-full overflow-hidden rounded-3xl bg-[#24272e] p-8 md:w-[400px]`}>
-      <div
-        onTouchStart={touchStart}
-        onTouchEnd={touchEnd}
-        className="relative flex h-[400px] items-center justify-center">
-        <div
-          style={{ transform: `translateX(-${selected * 120}px)` }}
-          className="absolute left-[120px] top-0 z-50 flex h-full w-full items-center justify-center gap-8 transition-all duration-300">
-          {categories.map((item, index) => {
-            return (
-              <div
-                key={item._id}
-                onClick={() => setSelected(index)}
-                className={`${categories[selected].name == item.name ? styles.logoCircle : styles.logoCircle2} grid h-[88px] min-h-[88px] w-[88px] min-w-[88px] cursor-pointer place-items-center rounded-full bg-[#24272e] transition-all delay-150 duration-0`}>
-                <img
-                  src={`assets/${item.name}.svg`}
-                  width={80}
-                  height={80}
-                  alt={item.name}
-                />
-              </div>
-            );
-          })}
-        </div>
-        <div className={`${styles.circleInCenter} relative z-50 h-28 w-28 rounded-full border-2 border-white/60 p-2.5`}>
-          <div className={`${styles.circle} absolute left-1/2 top-0 grid h-4 w-4 -translate-x-1/2 -translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
-            <div className="h-2 w-2 rounded-full bg-white"></div>
-          </div>
-          <div className={`${styles.circle} absolute bottom-0 left-1/2 grid h-4 w-4 -translate-x-1/2 translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
-            <div className="h-2 w-2 rounded-full bg-white"></div>
-          </div>
-          <div className="absolute left-1/2 top-0 h-[150px] w-[1px] -translate-x-1/2 -translate-y-full bg-gradient-to-t from-white/40"></div>
-          <div className="absolute bottom-0 left-1/2 h-[80px] w-[1px] -translate-x-1/2 translate-y-full bg-gradient-to-b from-white/40"></div>
-        </div>
-        <div className="absolute left-0 top-0 z-30 h-full w-full">
-          <div className="absolute left-1/2 top-1/2 aspect-square w-[320px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/10"></div>
-          <div className="absolute left-1/2 top-1/2 aspect-square w-[240px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/30 bg-white/5"></div>
-          <div className={`${styles.squareShadow} absolute left-1/2 top-1/2 h-[130px] w-9/12 -translate-x-1/2 -translate-y-[30px] rounded-full bg-[#24272e]`}></div>
-          <span className="absolute bottom-10 left-1/2 -translate-x-1/2 text-base font-normal text-white/80">{categories[selected].questionCount} QUESTIONS</span>
-        </div>
-      </div>
-      <div className="relative z-50">
-        <div className="mb-1 text-2xl font-semibold text-white/90">{categories[selected].name}</div>
-        <div className="mb-5 text-lg font-normal text-white/70">Test your {categories[selected].name} skills from basics to advanced!</div>
-        <button className={`${styles.button} w-fit rounded-full px-8 py-3`}>
-          <span className="text-lg font-medium text-white/80">Start quiz</span>
+    <div className="w-full md:w-[400px]">
+      <div className="relative z-10 mb-4 flex items-center justify-between px-4">
+        <button
+          onClick={goLeft}
+          className={`${styles.navigationBtn} overflow-hidden rounded-xl bg-[#24272e] px-12 py-2 text-base font-medium text-white/80`}>
+          Prev
         </button>
+        <button
+          onClick={goRight}
+          className={`${styles.navigationBtn} overflow-hidden rounded-xl bg-[#24272e] px-12 py-2 text-base font-medium text-white/80`}>
+          Next
+        </button>
+      </div>
+      <div className={`${styles.card} relative z-20 overflow-hidden rounded-3xl bg-[#24272e] p-8`}>
+        <div className="relative z-40 flex h-[400px] items-center justify-center">
+          <div
+            style={{ transform: `translateX(-${selected * 120}px)` }}
+            className="absolute left-[120px] top-0 z-50 flex h-full w-full items-center justify-center gap-8 transition-all duration-300">
+            {categories.map((item, index) => {
+              return (
+                <div
+                  key={item._id}
+                  onClick={() => setSelected(index)}
+                  className={`${categories[selected].name == item.name ? styles.logoCircle : styles.logoCircle2} grid h-[88px] min-h-[88px] w-[88px] min-w-[88px] cursor-pointer place-items-center rounded-full bg-[#24272e] transition-all delay-200 duration-0`}>
+                  <Image
+                    src={`assets/${item.name}.svg`}
+                    width={80}
+                    height={80}
+                    alt={item.name}
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div className={`${styles.circleInCenter} relative z-50 h-28 w-28 rounded-full border-2 border-white/60 p-2.5`}>
+            <div className="absolute left-0 top-0 h-full w-full">
+              <div className={`${styles.circle} absolute left-1/2 top-0 grid h-4 w-4 -translate-x-1/2 -translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
+                <div className="h-2 w-2 rounded-full bg-white"></div>
+              </div>
+              <div className={`${styles.circle} absolute bottom-0 left-1/2 grid h-4 w-4 -translate-x-1/2 translate-y-1/2 place-items-center rounded-full bg-[#24272e] p-1`}>
+                <div className="h-2 w-2 rounded-full bg-white"></div>
+              </div>
+            </div>
+            <div className="absolute left-1/2 top-0 h-[150px] w-[1px] -translate-x-1/2 -translate-y-full bg-gradient-to-t from-white/40"></div>
+            <div className="absolute bottom-0 left-1/2 h-[80px] w-[1px] -translate-x-1/2 translate-y-full bg-gradient-to-b from-white/40"></div>
+          </div>
+          <div className="absolute left-0 top-0 z-30 h-full w-full">
+            <div className="absolute left-1/2 top-1/2 aspect-square w-[320px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/10"></div>
+            <div className="absolute left-1/2 top-1/2 aspect-square w-[240px] -translate-x-1/2 -translate-y-1/2 rounded-full border-t border-white/30 bg-white/5"></div>
+            <div className={`${styles.squareShadow} absolute left-1/2 top-1/2 h-[130px] w-9/12 -translate-x-1/2 -translate-y-[30px] rounded-full bg-[#24272e]`}></div>
+            <div className={`${styles.questionCountShadow} absolute bottom-4 left-1/2 -translate-x-1/2 rounded-xl bg-[#3b3e43] px-5 py-2`}>
+              <span className="text-sm font-medium text-white/80">{categories[selected].questionCount} QUESTIONS</span>
+            </div>
+          </div>
+        </div>
+        <div className="relative z-50">
+          <div className="mb-1 text-2xl font-semibold text-white/90">{categories[selected].name}</div>
+          <div className="mb-5 text-lg font-normal text-white/70">Test your {categories[selected].name} skills from basics to advanced!</div>
+          <button
+            onClick={submit}
+            className={`${styles.button} w-fit rounded-full px-8 py-3 transition-all will-change-[box-shadow]`}>
+            <span className="text-lg font-medium text-white/80">Start quiz</span>
+          </button>
+        </div>
       </div>
     </div>
   );
