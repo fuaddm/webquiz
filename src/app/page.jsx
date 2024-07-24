a735cd83e618b1a88c216f1332e5edfda8a82e13
Fuad Damirchiev <fuad.damirchiyev@azerimed.com>
2024-07-24 16:22:35 +0400

    fetch from server

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,22 +3,11 @@ import Hero from '@/components/Hero';
 import Stars from '@/components/Stars';
 
 export default async function Home() {
-  // const resp = await fetch('http://127.0.0.1:3000/api/category');
-  // let data = null;
-  // if (resp.ok) {
-  //   data = await resp.json();
-  // }
-  const data = {
-    categories: [
-      { _id: '66979a6022116d3fc7f83676', name: 'HTML', questionCount: 3 },
-      { _id: '66979a8122116d3fc7f83678', name: 'CSS', questionCount: 0 },
-      {
-        _id: '66979a8822116d3fc7f8367a',
-        name: 'JAVASCRIPT',
-        questionCount: 0,
-      },
-    ],
-  };
+  const resp = await fetch('http://127.0.0.1:3000/api/category');
+  let data = null;
+  if (resp.ok) {
+    data = await resp.json();
+  }
   return (
     <div className="container mb-20 flex flex-col items-center pb-6 pt-10">
       <Header />
